ff85c7298467f9fd9310cf548b2a09aee65cd75b
Alysson Rocha <alysson.rocha@euentrego.com>
2025-05-31 22:17:15 -0300

    ajustando label do eixo

--- src/plot.js
+++ src/plot.js
@@ -1,24 +1,27 @@
 import * as d3 from 'd3';
 
 
-export async function loadChart(data, margens = { left: 50, right: 25, top: 25, bottom: 50 }) {
+export async function loadChart(data, margens = { left: 75, right: 50, top: 50, bottom: 75 }) {
     const svg = d3.select('svg');
 
 if (!svg) {
     return;
 }
 
+const svgWidth = +svg.style("width").split("px")[0] - margens.left - margens.right
+const svgHeight = +svg.style("height").split("px")[0] - margens.top - margens.bottom;
+
 // ---- Escalas
-const distExtent = d3.extent(data, d => d.hour);
-const mapX = d3.scaleBand()  // Mudança: scaleBand para barras
-    .domain(data.map(d => d.hour))  // Domínio com todos os valores de hora
-    .range([0, +svg.style("width").split("px")[0] - margens.left - margens.right])
-    .padding(0.1);  // Espaçamento entre as barras
+const mapX = d3.scaleBand()
+    .domain(data.map(d => d.hour))
+    .range([0, svgWidth])
+    .padding(0.1);
 
 const tipExtent = d3.extent(data, d => d.tip_amount);
+const maxTip = tipExtent[1];
 const mapY = d3.scaleLinear()
-    .domain([0, tipExtent[1]])  // Começar do 0 para barras
-    .range([+svg.style("height").split("px")[0] - margens.bottom - margens.top, 0]);
+    .domain([0, maxTip]) 
+    .range([svgHeight, 0]); // O eixo é invertido
 
 // ---- Eixos
 const xAxis = d3.axisBottom(mapX);
@@ -28,7 +31,14 @@ groupX.join('g')
     .attr('id', 'axisX')
     .attr('class', 'x axis')
     .attr('transform', `translate(${margens.left}, ${+svg.style('height').split('px')[0] - margens.bottom})`)
-    .call(xAxis);
+    .call(xAxis)
+    .append("text") // Label
+    .attr("x", svgWidth / 2)
+    .attr("y", 60)
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .style("font-size", "1.5em")
+    .text("Hora do Dia");;
 
 const yAxis = d3.axisLeft(mapY);
 const groupY = svg.selectAll('#axisY').data([0]);
@@ -37,23 +47,31 @@ groupY.join('g')
     .attr('id', 'axisY')
     .attr('class', 'y axis')
     .attr('transform', `translate(${margens.left}, ${margens.top})`)
-    .call(yAxis);
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(svgHeight / 2))
+    .attr("y", -70)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size", "1.5em")
+    .style("fill", "black")
+    .text("Gorjeta ($)");
 
-// ---- Barras (substituindo círculos)
 const selection = svg.selectAll('#group').data([0]);
 const cGroup = selection.join('g')
         .attr('id', 'group');
 
-const bars = cGroup.selectAll('rect')  // rect em vez de circle
+const bars = cGroup.selectAll('rect')
     .data(data);
 
 bars.enter()
     .append('rect')
     .attr('x', d => mapX(d.hour))
     .attr('y', d => mapY(d.tip_amount))
-    .attr('width', mapX.bandwidth())  // Largura da barra
-    .attr('height', d => +svg.style("height").split("px")[0] - margens.bottom - margens.top - mapY(d.tip_amount))  // Altura da barra
-    .attr('fill', 'steelblue');  // Cor das barras
+    .attr('width', mapX.bandwidth()) 
+    .attr('height', d => svgHeight - mapY(d.tip_amount))
+    .attr('fill', 'steelblue');
 
 bars.exit()
     .remove();
@@ -62,7 +80,7 @@ bars
     .attr('x', d => mapX(d.hour))
     .attr('y', d => mapY(d.tip_amount))
     .attr('width', mapX.bandwidth())
-    .attr('height', d => +svg.style("height").split("px")[0] - margens.bottom - margens.top - mapY(d.tip_amount))
+    .attr('height', d => svgHeight - mapY(d.tip_amount))
     .attr('fill', 'steelblue');
 
 d3.select('#group')
@@ -72,7 +90,7 @@ d3.select('#group')
 
 export function clearChart() {
     d3.select('#group')
-        .selectAll('circle')
+        .selectAll('rect')
         .remove();
 
     d3.select('#axisX')
